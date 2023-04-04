07176bf74223744a45a73a1dbee2f3b3222f4e77
andreykurzantsev <hawkeye54123@gmail.com>
2023-04-04 18:52:03 +0300

    add movement

---
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerEntity : MonoBehaviour
+    {
+        [Header("HorizontalMovement")]
+        [SerializeField] private float _horizontalSpeed;
+        [SerializeField] private bool _faceRight;
+
+        [Header("Jump")]
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private float _gravityScale;
+        [SerializeField] private Transform _groundCheck;
+        [SerializeField] private float _groundCheckRadius;
+        [SerializeField] private LayerMask _groundLayer;
+
+        private Rigidbody2D _rigidbody;
+        private bool _isOnGround;
+
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (!_isOnGround)
+            {
+                UpdateJump();
+            }
+        }
+
+        public void Jump()
+        {
+            if (!IsOnGround())
+            {
+                return;
+            }
+            _isOnGround = true;
+            _rigidbody.AddForce(Vector2.up * _jumpForce);
+            _rigidbody.gravityScale = _gravityScale;
+        }
+
+        private bool IsOnGround()
+        {
+            return Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+        }
+
+        private void UpdateJump()
+        {
+            _isOnGround = IsOnGround();
+
+            if (_isOnGround)
+            {
+                ResetJump();
+                return;
+            }
+        }
+
+        private void ResetJump()
+        {
+            _isOnGround = false;
+        }
+
+        public void MoveHorizontally(float direction)
+        {
+            SetDirection(direction);
+            Vector2 velocity = _rigidbody.velocity;
+            velocity.x = direction * _horizontalSpeed;
+            _rigidbody.velocity = velocity;
+        }
+
+        private void SetDirection(float direction)
+        {
+            if ((_faceRight && direction < 0) ||
+                (!_faceRight && direction > 0))
+            {
+                Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            transform.Rotate(0, 180, 0);
+            _faceRight = !_faceRight;
+        }
+
+    }
+}
